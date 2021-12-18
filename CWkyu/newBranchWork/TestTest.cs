c79ad387aad1638e1d5f0b0db8834a2a3df14a5a
Davis Doro(WS) <davisdoro@outlook.com>
2021-12-18 12:52:59 +0200

    branch changes

---
+++ CWkyu/newBranchWork/TestTest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CWkyu.newBranchWork
+{
+    internal class TestTest
+    {
+        public string TestClass { get; set; }
+    }
+}
